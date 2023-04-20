6a96170f06abf1b537dc46908cc8f3685d508dae
Haitam Moubal <hmoubal@e2r7p1.1337.ma>
2023-04-20 02:26:20 +0000

    starting ex01

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,7 +6,22 @@
 /*   By: hmoubal <hmoubal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:52:49 by hmoubal           #+#    #+#             */
-/*   Updated: 2023/04/18 12:52:50 by hmoubal          ###   ########.fr       */
+/*   Updated: 2023/04/20 01:57:13 by hmoubal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+
+class RPN
+{
+
+    public:
+        RPN(/* args */);
+        ~RPN();
+};
+
+#endif
