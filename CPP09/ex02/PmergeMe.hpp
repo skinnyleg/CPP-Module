20eca3a5c805102e20ef80f49c775310cd76caab
skinnyleg <haitamfox12@gmail.com>
2023-04-28 23:05:48 +0100

    starting on ex02

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,23 @@
 /*   By: hmoubal <hmoubal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:54:42 by hmoubal           #+#    #+#             */
-/*   Updated: 2023/04/18 12:54:43 by hmoubal          ###   ########.fr       */
+/*   Updated: 2023/04/25 17:16:22 by hmoubal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <deque>
+
+
+class PmergeMe
+{
+	public:
+		PmergeMe();
+		~PmergeMe();
+};
+
+#endif // ! PMERGEME_HPP
